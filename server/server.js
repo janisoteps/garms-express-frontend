0ca04b3cc070d90562c6a81cf886eb0d8fb481a7
Janis Dzikevics <jdzikevics@zmags.com>
2020-01-23 18:41:24 +0100

    build

--- server/server.js
+++ server/server.js
@@ -13,7 +13,6 @@ const sha1 = require('sha1');
 const aws = require('aws-sdk');
 
 
-// const api_base_url = 'http://18.203.186.0/api/';
 const api_base_url = 'https://main-api.garms.io/api/';
 // const api_base_url = 'http://127.0.0.1:5000/api/';
 const BUCKET_NAME = 'garms-userimages';
