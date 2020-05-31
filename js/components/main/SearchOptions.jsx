5bede3569a6a7d000648e739f2ea27570df9d140
janisoteps <janis.oteps@gmail.com>
2020-05-31 09:47:19 +0200

    cleanup

--- js/components/main/SearchOptions.jsx
+++ js/components/main/SearchOptions.jsx
@@ -3,8 +3,6 @@ import TextSearchBox from "../search/from_text/TextSearchBox";
 import {isMobile} from "react-device-detect";
 import {Route} from "react-router-dom";
 import ReactGA from "react-ga";
-import {SwatchesPicker} from "react-color";
-
 
 class SearchOptions extends React.Component {
     constructor(props) {
@@ -12,107 +10,9 @@ class SearchOptions extends React.Component {
         this.state = {
             sex: this.props.sex
         };
-
-        // this.focusOnTextBox = this.focusOnTextBox.bind(this);
-        // this.textSearchBox = React.createRef();
     }
 
-    // focusOnTextBox = () => {
-    //     this.textSearchBox.current.focusOnInput();
-    // }
     render() {
-        // const SelectedColorBubble = () => {
-        //     return (
-        //         <div
-        //             style={{
-        //                 position: 'absolute',
-        //                 width: '100%',
-        //                 top: 0,
-        //                 textAlign: 'center',
-        //                 zIndex: '11',
-        //                 marginTop: '-10px'
-        //             }}
-        //         >
-        //             {this.props.selectedColor !== null && (
-        //                 <div
-        //                     style={{
-        //                         width: '200px',
-        //                         height: '20px',
-        //                         backgroundColor: `rgba(${this.props.selectedColor[0]}, ${this.props.selectedColor[1]}, ${this.props.selectedColor[2]}, 1)`,
-        //                         marginLeft: 'calc(50vw - 100px)',
-        //                         borderWidth: '5px',
-        //                         border: 'solid white',
-        //                         borderRadius: '10px'
-        //                     }}
-        //                 >
-        //                 </div>
-        //             )}
-        //         </div>
-        //     )
-        // }
-        //
-        // const SWPicker = () => {
-        //     if (isMobile) {
-        //         return (
-        //             <SwatchesPicker
-        //                 onChange={ this.props.handleColorChange }
-        //                 width={'100%'}
-        //                 height={'calc(100vh - 250px)'}
-        //             />
-        //         )
-        //     } else {
-        //         return (
-        //             <SwatchesPicker
-        //                 onChange={ this.props.handleColorChange }
-        //                 width={'100%'}
-        //                 height={'calc(((1100 / 100vw) * 250)px)'}
-        //             />
-        //         )
-        //     }
-        // };
-        //
-        // const ColorPicker = () => {
-        //     if (isMobile) {
-        //         return (
-        //             <div
-        //                 style={{
-        //                     position: 'absolute',
-        //                     height: '500px',
-        //                     width: '100%',
-        //                     top: '250px',
-        //                     zIndex: '10',
-        //                     left: '0',
-        //                     textAlign: 'center'
-        //                 }}
-        //             >
-        //                 <SWPicker/>
-        //                 {this.props.selectedColor !== null && (
-        //                     <SelectedColorBubble />
-        //                 )}
-        //             </div>
-        //         )
-        //     } else {
-        //         return (
-        //             <div
-        //                 style={{
-        //                     position: 'absolute',
-        //                     height: '300px',
-        //                     width: '100%',
-        //                     top: '150px',
-        //                     zIndex: '10',
-        //                     left: '0',
-        //                     textAlign: 'center'
-        //                 }}
-        //             >
-        //                 <SWPicker/>
-        //                 {this.props.selectedColor !== null && (
-        //                     <SelectedColorBubble />
-        //                 )}
-        //             </div>
-        //         )
-        //     }
-        //
-        // }
 
         return (
             <div
